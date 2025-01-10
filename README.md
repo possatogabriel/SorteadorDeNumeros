68476d918a309335646aa87f05a97292e56d296d
possatogabriel <chaymitoyt@gmail.com>
2025-01-10 15:14:41 -0300

    Finish the project

--- README.md
+++ README.md
@@ -4,13 +4,13 @@
 
 💬 Primeiro projeto do último curso de Lógica de Programação da Alura
 
-🚩 **ESTADO ATUAL:** funcionalidade ao botão de _**Sortear**_ e toda a lógica por trás
+🚩 Este projeto apresenta uma **lógica em JS mais complexa**, com uso de **arrays, funções, estruturas de condição e repetição, DOM**, algumas alterações **_(completamente opcionais)_** nos arquivos de **HTML e CSS** para uma melhor vizualização do projeto...
 
-🔒 **A SER DESENVOLVIDO:** funcionalidade do botão de **_Reiniciar_** 
+🏆 O projeto foi realizado **_individualmente_**, buscando trazer soluções e respostas para problemas **_sem muita consulta aos materiais da aula_**
 
 ## 🔗 Link para vizualização
 
-<a href = "?"> <img alt="Static Badge" src="https://img.shields.io/badge/CLIQUE%20AQUI-LINK EM DESENVOLVIMENTO-blue?style=for-the-badge"> </a>
+<a href = "?"> <img alt="Static Badge" src="https://img.shields.io/badge/Clique aqui-blue?style=for-the-badge"> </a>
 
 ## 💻 Tecnologias Utilizadas
 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" height = "40"/> <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" height = "40"/> <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" height = "40" />
@@ -19,4 +19,4 @@
 [<img loading="lazy" src="https://avatars.githubusercontent.com/u/136634888?v=4" width=80><br> <sub> Gabriel Possato </sub>](https://github.com/possatogabriel)
 <br>
 <br>
-<p align = "center"> <img alt="Static Badge" src="https://img.shields.io/badge/STATUS%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20-em desenvolvimento-green?style=for-the-badge"> <br/> <img src = "img/alura1.png" height = "50"></p>
+<p align = "center"> <img alt="Static Badge" src="https://img.shields.io/badge/STATUS%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20-concluído-green?style=for-the-badge"> <br/> <img src = "img/alura1.png" height = "50"></p>
--- app.js
+++ app.js
@@ -8,13 +8,58 @@ function gerarNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-// FUNÇÃO PARA EXIBIR O TEXTO DOS NÚMEROS SORTEADOS
-function exibirNumeros(texto) {
-    let campo = document.querySelector("#resultado");
+// FUNÇÃO PARA EXIBIR O TEXTO DOS NÚMEROS SORTEADOS OU A MENSAGEM INICIAL
+function exibirTexto(tag, texto) {
+    let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-// FUNÇÃO AO CLICAR NO BOTÃO DE 'SORTEAR'
+// FUNÇÃO PARA LIMPAR OS 'INPUTS' AO REINICIAR O JOGO
+function limparCampo() {
+    let campoQuantidade = document.getElementById("quantidade");
+    let campoNumeroMinimo = document.getElementById("numeroMin");
+    let campoNumeroMaximo = document.getElementById("numeroMax");
+
+    campoQuantidade.value = "";
+    campoNumeroMinimo.value = "";
+    campoNumeroMaximo.value = "";
+}
+
+// FUNÇÃO QUE LIBERA UM BOTÃO
+function habilitarBotao(id) {
+    let botao = document.getElementById(id);
+
+    if (botao.classList.contains("container__botao-desabilitado")) {
+        botao.classList.remove("container__botao-desabilitado");
+        botao.classList.add("container__botao-habilitado");
+    }
+}
+
+// FUNÇÃO QUE BLOQUEIA UM BOTÃO
+function desabilitarBotao(id) {
+    let botao = document.getElementById(id);
+
+    if (botao.classList.contains("container__botao-habilitado")) {
+        botao.classList.remove("container__botao-habilitado");
+        botao.classList.add("container__botao-desabilitado");
+    }
+}
+
+// FUNÇÃO PARA REINICIAR O JOGO AO CLICAR NO BOTÃO 
+function clicarReiniciar() {
+    let mensagemInicial = "Números sorteados: nenhum até agora";
+
+    exibirTexto("#resultado", mensagemInicial);
+    exibirTexto("#final", "");
+    limparCampo();
+
+    habilitarBotao("btn-sortear");
+    desabilitarBotao("btn-reiniciar");
+
+    listaNumerosSorteados = [];
+}
+
+// FUNÇÃO PARA SORTEAR UM NÚMERO AO CLICAR NO BOTÃO
 function clicarSortear() {
     let qtndNumeros = parseInt(document.getElementById("quantidade").value);
     let numeroMinimo = parseInt(document.getElementById("numeroMin").value);
@@ -22,9 +67,27 @@ function clicarSortear() {
 
     let i;
 
+    // ESTRUTURA DE CONDIÇÃO PARA VERIFICAR SE O NÚMERO MÍNIMO É MAIOR QUE O NÚMERO MÁXIMO 
+    if (numeroMinimo >= numeroMaximo) {
+        alert("ERRO: O NÚMERO MÍNIMO não pode ser IGUAL OU MAIOR que o NÚMERO MÁXIMO.");
+        limparCampo();
+        return;
+    }
+
+    // ESTRUTURA DE CONDIÇÃO PARA VERIFICAR SE EXISTE A QUANTIDADE DE NÚMEROS A SEREM GERADOS DENTRE OS LIMITES ESTABELECIDOS
+    if (qtndNumeros > (numeroMaximo - numeroMinimo + 1)) {
+        alert("ERRO: A QUANTIDADE de números a serem gerados não pode ser MAIOR do que os NÚMEROS MÍNIMO E MÁXIMO.");
+        limparCampo();
+        return;
+    }
+
     // ESTRUTURA DE REPETIÇÃO PARA ARMAZENAR OS NÚMEROS ALEATÓRIOS NA LISTA
     for (i = 1; i <= qtndNumeros; i++) {
         let numerosSorteados = gerarNumeroAleatorio(numeroMinimo, numeroMaximo);
+
+        while (listaNumerosSorteados.includes(numerosSorteados)) {
+            numerosSorteados = gerarNumeroAleatorio(numeroMinimo, numeroMaximo);
+        }
 
         listaNumerosSorteados.push(numerosSorteados);
     }
@@ -35,6 +98,13 @@ function clicarSortear() {
     
     // CÓDIGO PARA MOSTRAR A MENSAGEM COM OS NÚMEROS SORTEADOS
     let mensagemNumerosSorteados = palavraNumeros + palavraSorteados + listaNumerosSorteados;
-    exibirNumeros(mensagemNumerosSorteados);
-}
+    exibirTexto("#resultado", mensagemNumerosSorteados);
 
+    habilitarBotao("btn-reiniciar");
+
+    // ESTRUTURA DE CONDIÇÃO QUE CONFIRMA SE É POSSÍVEL GERAR OUTRO NÚMEROS (se não, "encerra" o programa)
+    if (listaNumerosSorteados.length == numeroMaximo - numeroMinimo) {
+        exibirTexto("#final", "TODOS números possíveis foram gerados!");
+        desabilitarBotao("btn-sortear");
+    }
+}
